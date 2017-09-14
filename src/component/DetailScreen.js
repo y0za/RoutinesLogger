27894f726b3f9192808b44b9a92212f5419484ca
y0za <yoza.is12s@gmail.com>
2017-09-14 21:17:10 +0900

    Add LogCalendar to DetailScreen

--- src/component/DetailScreen.js
+++ src/component/DetailScreen.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+import LogCalendar from './LogCalendar';
+import { Calendar } from 'react-native-calendars';
+
+const doneDates = [
+  '2017-09-01',
+  '2017-09-02',
+  '2017-09-11',
+  '2017-09-12'
+];
 
 export default class DetailScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return { title: navigation.state.params.title }
   };
 
-
   render() {
     return (
-      <Text>Detail</Text>
+      <View style={{flex: 1}}>
+        <LogCalendar doneDates={doneDates} />
+      </View>
     );
   }
 }
