b1089f7da92e909d77f184d145f9787bf7dedcea
y0za <yoza.is12s@gmail.com>
2017-09-14 21:16:34 +0900

    Add LogCalendar component

---
+++ src/component/LogCalendar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Calendar } from 'react-native-calendars';
+
+export default class LogCalendar extends React.Component {
+  createMarkedDates(dates) {
+    const markedDates = {};
+    for (const date of dates) {
+      markedDates[date] = { marked: true };
+    }
+    return markedDates;
+  }
+
+  render() {
+    const markedDates = this.createMarkedDates(this.props.doneDates);
+
+    return (
+      <Calendar markedDates={markedDates} />
+    );
+  }
+}
