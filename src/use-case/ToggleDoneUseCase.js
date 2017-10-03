b60981aa28c22bb5e9f47ac25211cfc4af48eeab
y0za <yoza.is12s@gmail.com>
2017-10-03 23:37:59 +0900

    Add ToggleDoneUseCase

---
+++ src/use-case/ToggleDoneUseCase.js
@@ -0,0 +1,19 @@
+import { UseCase } from 'almin';
+import RoutineItem from '../domain/RoutineItem';
+import routineRepository from '../infra/RoutineRepository';
+
+export class ToggleDoneUseCase extends UseCase {
+  static create() {
+    return new ToggleDoneUseCase({ routineRepository });
+  }
+
+  constructor({ routineRepository }) {
+    this.routineRepository = routineRepository;
+  }
+
+  execute(routineId, date) {
+    const routineItem = this.routineRepository.findById(routineId);
+    routineItem.toggleDone(date);
+    this.routineRepository.store(routineItem);
+  }
+}
