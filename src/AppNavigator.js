a1a531ef75701f6f2f3f55d276003d9e35fa8d0a
y0za <yoza.is12s@gmail.com>
2017-10-08 19:31:49 +0900

    Add AddRoutineScreen

--- src/AppNavigator.js
+++ src/AppNavigator.js
@@ -1,8 +1,10 @@
 import { StackNavigator } from 'react-navigation';
 import ListScreen from './component/ListScreen';
 import DetailScreen from './component/DetailScreen';
+import AddRoutineScreen from './component/AddRoutineScreen';
 
 export default StackNavigator({
   List: { screen: ListScreen },
-  Detail: { screen: DetailScreen }
+  Detail: { screen: DetailScreen },
+  AddRoutine: { screen: AddRoutineScreen }
 });
---
+++ src/component/AddRoutineScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet
+} from 'react-native';
+import appLocator from '../AppLocator';
+import AddRoutineUseCase from '../use-case/AddRoutineUseCase';
+
+export default class ListScreen extends React.Component {
+  static navigationOptions = {
+    title: 'New Routine'
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      title: ''
+    }
+  }
+
+  addRoutine() {
+    appLocator.context.useCase(AddRoutineUseCase.create()).execute(
+      this.state.title
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          value={this.state.title}
+          onChangeText={(text) => this.setState({ title: text })}
+        />
+        <Button
+          title="Add Routine"
+          onPress={this.addRoutine.bind(this)}
+        />
+      </View>
+    );
+  }
+}
+
+
