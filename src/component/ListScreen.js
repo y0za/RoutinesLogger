c5f96d3bdbdb60c56a10048a35bf7af10cff31e4
y0za <yoza.is12s@gmail.com>
2017-10-08 14:24:27 +0900

    Change ListScreen to use screenProps and UseCase

--- src/component/ListScreen.js
+++ src/component/ListScreen.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import AlminReactContainer from 'almin-react-container';
 import RoutineList from './RoutineList';
+import appLocator from '../AppLocator';
+import ShowDetailUseCase from '../use-case/ShowDetailUseCase';
+import ToggleDoneUseCase from '../use-case/ToggleDoneUseCase';
 
 export default class ListScreen extends React.Component {
   static navigationOptions = {
     title: 'Routines',
   };
 
-  render() {
-    const { navigate } = this.props.navigation;
+  showDetail(routineId) {
+    appLocator.context.useCase(ShowDetailUseCase.create()).execute(routineId);
+  }
 
+  toggleDone(routineId) {
+    const date = this.props.screenProps.routineListState.date;
+    appLocator.context.useCase(ToggleDoneUseCase.create()).execute(routineId, date);
+  }
+
+  render() {
     return (
       <View>
-        <RoutineList navigate={navigate} />
+        <RoutineList
+          routines={this.props.screenProps.routineListState.routines}
+          showDetail={this.showDetail}
+          toggleDone={this.toggleDone.bind(this)}
+        />
       </View>
     );
   }
