f933f6f9987b7ffb2eefa7fe2eb5a1304015787e
y0za <yoza.is12s@gmail.com>
2017-10-08 14:24:11 +0900

    Change RoutineList component to use props

--- src/component/RoutineList.js
+++ src/component/RoutineList.js
@@ -2,22 +2,20 @@ import React from 'react';
 import { FlatList } from 'react-native';
 import RoutineItem from './RoutineItem';
 
-const defaultRoutines = [
-  { id: 1, title: 'Walking', done: true },
-  { id: 2, title: 'Reading', done: false },
-  { id: 3, title: 'Cleaning', done: false }
-];
-
 export default class RoutineList extends React.Component {
   render() {
-    const onPressItem = (title) => {
-      this.props.navigate('Detail', { title: title });
-    };
-
     return (
       <FlatList
-        data={defaultRoutines}
-        renderItem={({item}) => <RoutineItem routine={item} onPressItem={onPressItem.bind(this)} />}
+        data={this.props.routines}
+        renderItem={({item}) => {
+          return (
+            <RoutineItem
+              routine={item}
+              showDetail={this.props.showDetail}
+              toggleDone={this.props.toggleDone}
+            />
+          );
+        }}
         keyExtractor={(item, index) => item.id}
       />
     );
