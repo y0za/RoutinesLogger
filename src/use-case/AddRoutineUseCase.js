57ed2b9888a60401bb541a256ab7938590374137
y0za <yoza.is12s@gmail.com>
2017-10-11 20:08:09 +0900

    Add back navigation to the end of AddRoutineUseCase#execute

--- src/use-case/AddRoutineUseCase.js
+++ src/use-case/AddRoutineUseCase.js
@@ -1,16 +1,22 @@
 import { UseCase } from 'almin';
+import { NavigationActions } from 'react-navigation';
 import RoutineItem from '../domain/RoutineItem';
 import routineRepository from '../infra/RoutineRepository';
 import SimpleDate from '../domain/value/SimpleDate';
+import ChangeNavigationUseCase from './ChangeNavigationUseCase';
 
 export default class AddRoutineUseCase extends UseCase {
   static create() {
-    return new AddRoutineUseCase({ routineRepository });
+    return new AddRoutineUseCase({
+      routineRepository,
+      navigationActions: NavigationActions
+    });
   }
 
-  constructor({ routineRepository }) {
+  constructor({ routineRepository, navigationActions }) {
     super();
     this.routineRepository = routineRepository;
+    this.navigationActions = navigationActions;
   }
 
   execute(title, sinceDate = SimpleDate.fromDate()) {
@@ -21,5 +27,8 @@ export default class AddRoutineUseCase extends UseCase {
     });
 
     this.routineRepository.store(routineItem);
+
+    const backAction = this.navigationActions.back();
+    this.context.useCase(new ChangeNavigationUseCase()).execute(backAction);
   }
 }
